63d3e02fda2f5f2605e81a0ada4b479c42d3a4c4
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-04-08 01:13:03 +0530

    Added new activity file for save meals to db

--- EasyMeal/app/src/main/java/com/example/easymeal/MainActivity.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.easymeal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnAddMealsDB : Button
+    private lateinit var btnSearchByIngredient : Button
+    private lateinit var btnSearchByMeal : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAddMealsDB = findViewById(R.id.btnAddMeal)
+        btnSearchByIngredient = findViewById(R.id.btnSearchIngredient)
+        btnSearchByMeal = findViewById(R.id.btnSearchMeal)
+
+        btnAddMealsDB.setOnClickListener { addMealsToDB() }
+    }
+
+    private fun addMealsToDB(){
+        val openAddMealsToDB = Intent(this, AddMealsToDB::class.java)
+        startActivity(openAddMealsToDB)
     }
 }
