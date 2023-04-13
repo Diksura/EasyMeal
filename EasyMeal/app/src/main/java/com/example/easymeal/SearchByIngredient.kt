a1c54c8c03fac996bf75289bac402a1d3db403a3
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-04-14 01:50:43 +0530

    Added network status checking method

--- EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
@@ -1,11 +1,26 @@
 package com.example.easymeal
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SearchByIngredient : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_by_ingredient)
+
+        Log.i("testNet", "Network Stat : ${networkAvailability()}")
+    }
+
+
+    // For check network status if users is online of not
+    @Suppress("DEPRECATION")
+    private fun networkAvailability(): Boolean{
+        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        return networkInfo?.isConnectedOrConnecting?:false
     }
 }
