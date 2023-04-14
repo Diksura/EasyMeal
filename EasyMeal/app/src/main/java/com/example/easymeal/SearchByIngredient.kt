903f27e126d02bcf0c4edc801e84a1186a46669c
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-04-14 22:59:52 +0530

    Added to real JSOn in web page

--- EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
@@ -1,17 +1,92 @@
 package com.example.easymeal
 
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
+import com.example.easymeal.database.Meal
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SearchByIngredient : AppCompatActivity() {
+
+    var mealsArr = arrayListOf<Meal>()
+
+    lateinit var btnSearch: Button
+    lateinit var btnSaveMeals: Button
+    lateinit var edTxtSearchBar: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_by_ingredient)
 
+        btnSearch = findViewById(R.id.btnRetrieveMeals)
+        btnSaveMeals = findViewById(R.id.btnSaveMeals)
+        edTxtSearchBar = findViewById(R.id.inputSearchByIngredient)
+
+        btnSaveMeals.isVisible = false
+
+        btnSearch.setOnClickListener {  }
+
         Log.i("testNet", "Network Stat : ${networkAvailability()}")
+        readFromWeb()
+    }
+
+
+    fun readFromWeb(){
+        var stringBuilder = StringBuilder()
+
+        var url_string = "https://www.themealdb.com/api/json/v1/1/search.php?s=rice"
+        val url = URL(url_string)
+        val connect : HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        runBlocking {
+            launch {
+                withContext(Dispatchers.IO){
+                    var bufReader = BufferedReader(InputStreamReader(connect.inputStream))
+                    var line: String? = bufReader.readLine()
+
+                    while (line!= null){
+                        stringBuilder.append(line + "\n")
+                        line = bufReader.readLine()
+                    }
+                    // adding parse method
+                    parseJSON(stringBuilder)
+                }
+            }
+        }
+
+    }
+
+    suspend fun parseJSON(stb: java.lang.StringBuilder){
+
+        val json = JSONObject(stb.toString())
+        val allMeals = java.lang.StringBuilder()
+
+        var jsonArray: JSONArray = json.getJSONArray("meals")
+
+        allMeals.append(jsonArray)
+        Log.i("allBooks", allMeals.toString())
+
+        val tv: TextView = findViewById(R.id.tv)
+        tv.setText(allMeals)
     }
 
 
