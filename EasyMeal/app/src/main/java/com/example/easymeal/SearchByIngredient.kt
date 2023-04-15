247979b9f6fda1d201155575c072cc5c6658c4ae
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-04-16 00:31:20 +0530

    added retrieved data to Meals data class and add to arraylist

--- EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
@@ -87,8 +87,73 @@ class SearchByIngredient : AppCompatActivity() {
         allMeals.append(jsonArray)
         Log.i("allBooks", allMeals.toString())
 
+        // adding all json string to list
         val tv: TextView = findViewById(R.id.tv)
         tv.setText(allMeals)
+
+        //adding single data to Meal class
+        for (i in 0 until jsonArray.length()){
+            val meal: JSONObject = jsonArray[i] as JSONObject
+
+            val singleMeal = Meal(
+                idMeal = meal["idMeal"] as? Int,
+                strMeal = meal["strMeal"] as? String,
+                strDrinkAlternate = meal["strDrinkAlternate"] as? String,
+                strCategory = meal["strCategory"] as? String,
+                strArea = meal["strArea"] as? String,
+                strInstructions = meal["strInstructions"] as? String,
+                strMealThumb = meal["strMealThumb"] as? String,
+                strTags = meal["strTags"] as? String,
+                strYoutube = meal["strYoutube"] as? String,
+                strIngredient1 = meal["strIngredient1"] as? String,
+                strIngredient2 = meal["strIngredient2"] as? String,
+                strIngredient3 = meal["strIngredient3"] as? String,
+                strIngredient4 = meal["strIngredient4"] as? String,
+                strIngredient5 = meal["strIngredient5"] as? String,
+                strIngredient6 = meal["strIngredient6"] as? String,
+                strIngredient7 = meal["strIngredient7"] as? String,
+                strIngredient8 = meal["strIngredient8"] as? String,
+                strIngredient9 = meal["strIngredient9"] as? String,
+                strIngredient10 = meal["strIngredient10"] as? String,
+                strIngredient11 = meal["strIngredient11"] as? String,
+                strIngredient12 = meal["strIngredient12"] as? String,
+                strIngredient13 = meal["strIngredient13"] as? String,
+                strIngredient14 = meal["strIngredient14"] as? String,
+                strIngredient15 = meal["strIngredient15"] as? String,
+                strIngredient16 = meal["strIngredient16"] as? String,
+                strIngredient17 = meal["strIngredient17"] as? String,
+                strIngredient18 = meal["strIngredient18"] as? String,
+                strIngredient19 = meal["strIngredient19"] as? String,
+                strIngredient20 = meal["strIngredient20"] as? String,
+                strMeasure1 = meal["strMeasure1"] as? String,
+                strMeasure2 = meal["strMeasure1"] as? String,
+                strMeasure3 = meal["strMeasure1"] as? String,
+                strMeasure4 = meal["strMeasure1"] as? String,
+                strMeasure5 = meal["strMeasure1"] as? String,
+                strMeasure6 = meal["strMeasure1"] as? String,
+                strMeasure7 = meal["strMeasure1"] as? String,
+                strMeasure8 = meal["strMeasure1"] as? String,
+                strMeasure9 = meal["strMeasure1"] as? String,
+                strMeasure10 = meal["strMeasure1"] as? String,
+                strMeasure11 = meal["strMeasure1"] as? String,
+                strMeasure12 = meal["strMeasure1"] as? String,
+                strMeasure13 = meal["strMeasure1"] as? String,
+                strMeasure14 = meal["strMeasure1"] as? String,
+                strMeasure15 = meal["strMeasure1"] as? String,
+                strMeasure16 = meal["strMeasure1"] as? String,
+                strMeasure17 = meal["strMeasure1"] as? String,
+                strMeasure18 = meal["strMeasure1"] as? String,
+                strMeasure19 = meal["strMeasure1"] as? String,
+                strMeasure20 = meal["strMeasure1"] as? String,
+                strSource = meal["strSource"] as? String,
+                strImageSource = meal["strImageSource"] as? String,
+                strCreativeCommonsConfirmed = meal["strCreativeCommonsConfirmed"] as? String,
+                dateModified = meal["dateModified"] as? String,
+                )
+
+            mealsArr.add(singleMeal)
+
+        }
     }
 
 
@@ -108,6 +173,15 @@ class SearchByIngredient : AppCompatActivity() {
             dialog.setCancelable(false)
             dialog.setContentView(R.layout.network_erroe_popup)
             dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val btnRetry: Button = dialog.findViewById(R.id.btnRetry)
+            val btnBack: Button = dialog.findViewById(R.id.btnBack)
+
+            btnRetry.setOnClickListener {
+                if (!networkAvailability()){
+                    dialog.dismiss()
+                }
+            }
 
             dialog.show()
         }
