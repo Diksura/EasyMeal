d2d115aeac50b37fbd960e03b6402f10ff7d0eb8
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-04-21 20:11:51 +0530

    Add a new query

--- EasyMeal/app/src/main/java/com/example/easymeal/database/MealsDao.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/database/MealsDao.kt
@@ -8,6 +8,9 @@ interface MealsDao {
     @Query("SELECT * FROM mealsTable")
     suspend fun getAll(): List<Meal>
 
+    @Query("SELECT * FROM mealsTable WHERE strMeal LIKE :searchNm")
+    suspend fun getSearchMeals(searchNm: String): List<Meal>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMeal(meal: Meal)
 
