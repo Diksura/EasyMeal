f0810809ce9eb685c48ebb9fb0a8b2f3e8fdb44f
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-05-13 10:14:32 +0530

    Added card view to search results

--- EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/SearchByIngredient.kt
@@ -11,13 +11,14 @@ import android.util.Log
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.example.easymeal.dataAdding.MealsList
 import com.example.easymeal.database.Meal
 import com.example.easymeal.database.MealsDatabase
 import com.example.easymeal.repository.UtilityRepository
+import com.example.easymeal.resultMealsView.ResultsActivityAdaptor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -58,7 +59,13 @@ class SearchByIngredient : AppCompatActivity() {
 
         btnSearch.setOnClickListener {
             readFromWeb()
-            viewMeals()
+//            viewMeals()
+
+            val recyclerView: RecyclerView = findViewById(R.id.searchByIngRclView)
+
+            val adapter = ResultsActivityAdaptor(this, mealsArr)
+            recyclerView.adapter = adapter
+
             btnSaveMeals.isVisible = true
         }
 
@@ -196,7 +203,7 @@ class SearchByIngredient : AppCompatActivity() {
 
     }
 
-    fun viewMeals(){
+/*    fun viewMeals(){
         val tv: TextView = findViewById(R.id.tv)
 
         runBlocking {
@@ -210,7 +217,7 @@ class SearchByIngredient : AppCompatActivity() {
                 tv.text = mealsText.toString()
             }
         }
-    }
+    }*/
 
 
 
