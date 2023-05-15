8fd865b2864657fadbd039c186160349182d0129
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-05-15 12:22:20 +0530

    Update MealsDao.kt

--- EasyMeal/app/src/main/java/com/example/easymeal/database/MealsDao.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/database/MealsDao.kt
@@ -8,7 +8,27 @@ interface MealsDao {
     @Query("SELECT * FROM mealsTable")
     suspend fun getAll(): List<Meal>
 
-    @Query("SELECT * FROM mealsTable WHERE strMeal LIKE :mealName")
+    @Query("SELECT * FROM mealsTable WHERE lower(strIngredient1) LIKE '%' || :mealName || '%' " +
+            "OR lower(strIngredient2) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient3) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient4) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient5) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient6) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient7) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient8) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient9) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient10) LIKE '%' || lower(:mealName) || '%' "+
+            "OR lower(strIngredient11) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient12) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient13) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient14) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient15) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient16) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient17) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient18) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient19) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strIngredient20) LIKE '%' || lower(:mealName) || '%' " +
+            "OR lower(strMeal) LIKE '%' || lower(:mealName) || '%' ")
     suspend fun getSearchMeals(mealName: String): List<Meal>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
