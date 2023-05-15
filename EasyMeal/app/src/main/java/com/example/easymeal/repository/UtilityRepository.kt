2d75b4f7f9c926d57091e6039dfc3a8dde4b7b37
Pasan Diksura <pasan.20210415@iit.ac.lk>
2023-05-15 06:13:26 +0530

    Update UtilityRepository.kt

--- EasyMeal/app/src/main/java/com/example/easymeal/repository/UtilityRepository.kt
+++ EasyMeal/app/src/main/java/com/example/easymeal/repository/UtilityRepository.kt
@@ -1,10 +1,55 @@
 package com.example.easymeal.repository
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.ConnectivityManager
+import android.view.Window
+import android.widget.Button
 import android.widget.Toast
+import com.example.easymeal.MainActivity
+import com.example.easymeal.R
 
-class UtilityRepository {
-    fun makeToast(context: Context, message: String, isShort: Boolean){
+class UtilityRepository(val context: Context) {
+
+    // For check network status if users is online of not
+    fun networkCheckAndGiveError(){
+        if (!networkAvailability()){
+            networkNotAvailableError()
+        }
+    }
+    @Suppress("DEPRECATION")
+    private fun networkAvailability(): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+
+        return networkInfo?.isConnectedOrConnecting?:false
+    }
+
+    private fun networkNotAvailableError(){
+        if (!networkAvailability()){
+            val dialog = Dialog(context)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.network_error_popup)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val btnRetry: Button = dialog.findViewById(R.id.btnRetry)
+            val btnBack: Button = dialog.findViewById(R.id.btnBack)
+
+            btnRetry.setOnClickListener {
+                if (networkAvailability()){
+                    dialog.dismiss()
+                }
+            }
+
+            dialog.show()
+        }
+    }
+
+    fun makeToast(message: String, isShort: Boolean){
         if (isShort) {
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
         }
